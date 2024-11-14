a1ea7bd5332692a97ed600573597b3c5e24b0f08
Abhishek <abhisheklitlost@gmail.com>
2024-11-14 12:41:57 +0530

    fixed error of validating token

--- src/main/java/com/abhishek/Security/config/JwtFilter.java
+++ src/main/java/com/abhishek/Security/config/JwtFilter.java
@@ -40,7 +40,7 @@ public class JwtFilter extends OncePerRequestFilter {
 			username = jwtService.extractUsername(token);
 		}
 		
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 			UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(username);
 			if (jwtService.validateToken(token, userDetails)) {
 				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
--- src/main/java/com/abhishek/Security/config/SecurityConfig.java
+++ src/main/java/com/abhishek/Security/config/SecurityConfig.java
@@ -11,8 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 // import org.springframework.security.crypto.password.NoOpPasswordEncoder;
